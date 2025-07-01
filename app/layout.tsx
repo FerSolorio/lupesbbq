c8ebdb65f3e9a949e16cbbc38df6206b344b071a
FerSolorio <elia.fsolorio@gmail.com>
2025-07-01 12:16:37 -0600

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,17 @@
+// app/layout.tsx
+import React from 'react';
+
+export const metadata = {
+  title: 'Tu Título',
+  description: 'Descripción de tu aplicación',
+};
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <html lang="es">
+      <body>{children}</body>
+    </html>
+  );
+};
+
+export default RootLayout;
